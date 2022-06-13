cae82c9cea1a8940668328ddd115e8ec8379b126
mesutfd <106558393+mesutfd@users.noreply.github.com>
2022-06-13 15:55:27 +0430

    Fixing increase/decrease product count.

--- static/js/custom.js
+++ static/js/custom.js
@@ -77,3 +77,12 @@ function removeOrderDetail(detailId){
         }
     })
 }
+
+
+function changeOrderDetailCount(detailId, state) {
+    $.get('/user/change-order-detail?detail_id=' + detailId + '&state=' + state).then(res => {
+        if (res.status === 'success') {
+            $('#order-detail-content').html(res.body);
+        }
+    });
+}
--- user_panel_module/urls.py
+++ user_panel_module/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('edit-profile', EditUserProfilePage.as_view(), name='edit_profile_page'),
     path('user-basket', user_basket, name='user_basket_page'),
     path('remove-order-detail', remove_order_detail, name='remove_order_detail_ajax'),
+    path('change-order-detail', change_order_detail_count, name='change_order_detail_count_ajax'),
 ]
